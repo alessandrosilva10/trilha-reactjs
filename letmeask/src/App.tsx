9dd259a729e3d1df932fb5840cd6fda79adbf862
Alessandro <alessandro.mpgh@gmail.com>
2021-10-07 00:02:08 -0300

    Mission ReactJS Aula 01  Liftoff

---
+++ letmeask/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button text="Increment" />
+    </div>
+  );
+}
+
+export default App;
---
+++ letmeask/src/services/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_API_KEY,
+  authDomain: process.env.REACT_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_DATABASE_URL,
+  projectId: process.env.REACT_PROJECT_ID,
+  storageBucket: process.env.REACT_PROJECT_BUCKET,
+  messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const database = getDatabase(app);
+export default app;
